0df1ecb02f156866ec2f475ab069f1b445cabbc6
Cafer Tura Çetin <cafercetin.tr@gmail.com>
2026-07-25 11:41:10 +0300

    feat: add register address macros for INA226 implementation

--- scr/ina226.c
+++ scr/ina226.c
@@ -1,2 +1,18 @@
 #include "ina226.h"
 
+/**
+ * @brief Register address macros for INA226.
+ * 
+ */
+#define INA226_CONFIG_REG                   (0x00)
+#define INA226_SH_VOLTAGE_REG               (0x01)
+#define INA226_BUS_VOLTAGE_REG              (0x02)
+#define INA226_POWER_REG                    (0x03)
+#define INA226_CURRENT_REG                  (0x04)
+#define INA226_CALIBRATION_REG              (0x05)
+#define INA226_MASK_EN_REG                  (0x06)
+#define INA226_ALERT_LIM_REG                (0x07)
+#define INA226_MANCUFACTURE_ID_REG          (0xFE)
+#define INA226_DIE_ID_REG                   (0xFF)
+
+
