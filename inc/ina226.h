f6c679134f8e685d0ae7fcbaa453deb00d3cc48c
Cafer Tura Çetin <cafercetin.tr@gmail.com>
2026-07-25 16:17:07 +0300

    refactor: rename configuration masks for clarity in INA226 header

--- inc/ina226.h
+++ inc/ina226.h
@@ -122,20 +122,20 @@ typedef uint16_t INA226_Alert_Func_t;
  *
  *        These masks are used when packing field values into the 16-bit registers.
  */
-#define INA226_CFG_RESET_MASK          (0x8000U)
-#define INA226_CFG_RESET_POS           (15U)
+#define INA226_CONFIG_RESET_MASK          (0x8000U)
+#define INA226_CONFIG_RESET_POS           (15U)
 
-#define INA226_CFG_AVG_MASK            (0x0E00U) // Bits 11-9 (0000 1110 0000 0000)
-#define INA226_CFG_AVG_POS             (9U)
+#define INA226_CONFIG_AVG_MASK            (0x0E00U) // Bits 11-9 (0000 1110 0000 0000)
+#define INA226_CONFIG_AVG_POS             (9U)
 
-#define INA226_CFG_BUS_CT_MASK         (0x01C0U) // Bits 8-6 (0000 0001 1100 0000)
-#define INA226_CFG_BUS_CT_POS          (6U)
+#define INA226_CONFIG_BUS_CT_MASK         (0x01C0U) // Bits 8-6 (0000 0001 1100 0000)
+#define INA226_CONFIG_BUS_CT_POS          (6U)
 
-#define INA226_CFG_SHUNT_CT_MASK       (0x0038U) // Bits 5-3 (0000 0000 0011 1000)
-#define INA226_CFG_SHUNT_CT_POS        (3U)
+#define INA226_CONFIG_SHUNT_CT_MASK       (0x0038U) // Bits 5-3 (0000 0000 0011 1000)
+#define INA226_CONFIG_SHUNT_CT_POS        (3U)
 
-#define INA226_CFG_MODE_MASK           (0x0007U) // Bits 2-0 (0000 0000 0000 0111)
-#define INA226_CFG_MODE_POS            (0U)
+#define INA226_CONFIG_MODE_MASK           (0x0007U) // Bits 2-0 (0000 0000 0000 0111)
+#define INA226_CONFIG_MODE_POS            (0U)
 
 #define INA226_MASK_ENABLE_ALERT_FUNC_MASK   (0xFC00U) // Bits 15-10
 
